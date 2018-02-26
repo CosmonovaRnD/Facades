cbae6774119981e577cebca2d8b576b643ceb6b4
Serhii Kondratiuk <serhii.kondratiuk@cosmonova.net>
2018-02-26 15:24:44 +0200

    cdn facade

---
+++ src/CDN/CDNException.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace CosmonovaRnD\Facades\CDN;
+
+/**
+ * Class CDNException
+ *
+ * @author Serhii Kondratiuk <serhii.kondratiuk@cosmonova.net>
+ * @package CosmonovaRnD\Facades\Buildings
+ * @since   1.4.0
+ * Cosmonova | Research & Development
+ */
+class CDNException extends \Exception
+{
+
+}
---
+++ src/CDN/DTO/FileUpload.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+
+namespace CosmonovaRnD\Facades\CDN\DTO;
+
+/**
+ * Class FileUpload
+ *
+ * @author Serhii Kondratiuk <serhii.kondratiuk@cosmonova.net>
+ * @package CosmonovaRnD\Facades\CDN\DTO
+ * @since   1.4.0
+ * Cosmonova | Research & Development
+ */
+class FileUpload
+{
+    /**
+     * @var string
+     */
+    public $uuid;
+    /**
+     * @var string
+     */
+    public $href;
+    /**
+     * @var string
+     */
+    public $type;
+    /**
+     * @var int
+     */
+    public $size;
+
+    /**
+     * FileUpload constructor.
+     * @param string $uuid
+     * @param string $href
+     * @param string $type
+     * @param int $size
+     */
+    public function __construct(
+        string $uuid,
+        string $href,
+        string $type,
+        int $size
+    ) {
+        $this->uuid = $uuid;
+        $this->href = $href;
+        $this->type = $type;
+        $this->size = $size;
+    }
+
+    /**
+     * @param array $data
+     * @return FileUpload
+     */
+    public static function createFromResponse(array $data): self
+    {
+        return new self(
+            $data['uuid'],
+            $data['href'],
+            $data['type'],
+            (int)$data['size']
+        );
+    }
+}
